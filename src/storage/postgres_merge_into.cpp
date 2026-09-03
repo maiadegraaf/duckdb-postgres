67d46393bbd9b39d8fa4222a3bca42995034b9fc
Carlo Piovesan <piovesan.carlo@gmail.com>
2026-09-03 12:53:30 +0200

    Format fix

--- src/storage/postgres_merge_into.cpp
+++ src/storage/postgres_merge_into.cpp
@@ -116,8 +116,8 @@ PhysicalOperator &PostgresCatalog::PlanMergeInto(ClientContext &context, Physica
 		actions.emplace(entry.first, std::move(planned_actions));
 	}
 
-	return planner.Make<PhysicalMergeInto>(op.types, plan, std::move(actions), op.row_id_start, op.source_marker,
-	                                       false, op.return_chunk);
+	return planner.Make<PhysicalMergeInto>(op.types, plan, std::move(actions), op.row_id_start, op.source_marker, false,
+	                                       op.return_chunk);
 }
 
 } // namespace duckdb
